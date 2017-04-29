46db66b1e5560d190ae8aa63e2102d5615da7735
Max Hunter <max_phunter@yahoo.com>
2017-04-29 12:05:52 -0400

    jshintified

--- quadraticLogic.js
+++ quadraticLogic.js
@@ -7,27 +7,33 @@ $('input').keyup(function() {
     var b = $('#b').val();
     var c = $('#c').val();
 
-    squareRootContents = (b * b) - (4 * a * c);
+    var squareRootContents = (b * b) - (4 * a * c);
+    var squareRoot;
+    var valueOne;
+    var valueTwo;
+    var partOneString;
+    var partTwoString;
+    var imaginarySquareRootContents;
 
     if (squareRootContents >= 0) {
         imaginary = false;
-        var squareRoot = Math.sqrt(squareRootContents);
-        var valueOne = ((-b) + squareRoot) / (2 * a);
-        var valueTwo = ((-b) - squareRoot) / (2 * a);
+        squareRoot = Math.sqrt(squareRootContents);
+        valueOne = ((-b) + squareRoot) / (2 * a);
+        valueTwo = ((-b) - squareRoot) / (2 * a);
     } else {
         imaginary = true;
         //there are going to be imaginary roots
 
         //factor out the -i
-        var imaginarySquareRootContents = squareRootContents * -1
-        var squareRoot = Math.sqrt(imaginarySquareRootContents);
-        var partOneString = -b / (2 * a);
+        imaginarySquareRootContents = squareRootContents * -1;
+        squareRoot = Math.sqrt(imaginarySquareRootContents);
+        partOneString = -b / (2 * a);
 
         //if the square root will leave a bunch of decimal places, we want to format it under a radical...
         if (squareRoot % 1 !== 0) {
-            var partTwoString = "i" + "\u221A" + imaginarySquareRootContents + "/" + (2 * a);
+            partTwoString = "i" + "\u221A" + imaginarySquareRootContents + "/" + (2 * a);
         } else {
-            var partTwoString = (squareRoot / (2 * a))+"i";
+            partTwoString = (squareRoot / (2 * a))+"i";
         }
 
     }
